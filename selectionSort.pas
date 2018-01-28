c94fba61b161f58791ec53b53391f787eb986e64
Fikri Adriansa <fikriadriansa21@gmail.com>
2018-01-28 19:30:57 +0700

    commit pertama

---
+++ selectionSort.pas
@@ -0,0 +1,70 @@
+program SelectionSorting;
+const
+     maks_data = 20;
+type
+    array_data = array[1..maks_data] of integer;
+var
+   data : array_data;
+   N : integer;
+
+   procedure IsiData(var data: array_data);
+   var
+    i: integer;
+   begin
+        write('Masukkan Jumlah Data : '); readln(N);
+        for i:=1 to N do
+        begin
+             write('Data ke[',i,'] : '); readln(data[i]);
+        end;
+   end;
+
+   //procedure tukar_data(var data1,data2 : integer);
+   //var
+   //   temp : integer;
+   //begin
+   //     temp := data1;
+   //     data1 := data2;
+   //     data2 := temp;
+   //end;
+
+   procedure selection_sort_max(var data: array_data);
+   var
+      i,j,max,temp: integer;
+   begin
+        for i:=1 to (N-1) do
+        max := 1;
+        begin
+             for j:= 2 to (N+1)-i do
+             begin
+                  if(data[j] > data[max])
+                    then
+                        max := j;
+             end;
+             temp := data[j];
+             data[j] := data[max];
+             data[max] := temp;
+        end;
+   end;
+
+   procedure TampilData(var data: array_data);
+   var
+      i: integer;
+   begin
+        for i:=1 to N do
+        begin
+             write(data[i]);
+             writeln(' ');
+        end;
+   end;
+begin
+   IsiData(data);
+   writeln('Data Sebelum diurutkan');
+   TampilData(data);
+   writeln();
+
+   selection_sort_max(data);
+   writeln('Data Diurutkan Berdasarkan yang Paling Tinggi');
+   writeln();
+   TampilData(data);
+   readln;
+end.
